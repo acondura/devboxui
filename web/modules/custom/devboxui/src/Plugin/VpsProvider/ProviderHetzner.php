0b0f48a325d3849f097a1086d414692114b28082
Andrei Condurachi <andrei@condurachi.ro>
2025-07-19 16:02:47 +0300

    updates

--- web/modules/custom/devboxui/src/Plugin/VpsProvider/ProviderHetzner.php
+++ web/modules/custom/devboxui/src/Plugin/VpsProvider/ProviderHetzner.php
@@ -17,35 +17,6 @@ class ProviderHetzner extends VpsProviderPluginBase {
       'name' => 'Hetzner',
       'api_url' => 'https://api.hetzner.cloud/v1',
       'currency' => 'EUR',
-      'server_types' => 'server_types',
-      'server_types_opts' => [
-        'architecture' => 'architecture',
-        'cores' => 'cores',
-        'cpu_type' => 'cpu_type',
-        'description' => 'description',
-        'disk' => 'disk',
-        'id' => 'id',
-        'memory' => 'memory',
-        'prices' => 'prices',
-      ],
-      'images' => 'images',
-      'images_opts' => [
-        'id' => 'id',
-        'description' => 'description',
-        'type' => 'system',
-        'status' => 'available',
-        'os_flavor' => 'ubuntu',
-        'sort' => 'name:desc',
-        'architecture' => 'x86',
-        'per_page' => '1',
-      ],
-      'locations' => 'locations',
-      'locations_opts' => [
-        'id' => 'id',
-        'city' => 'city',
-        'country' => 'country',
-        'network_zone' => 'network_zone',
-      ],
     ];
   }
 
@@ -81,19 +52,45 @@ class ProviderHetzner extends VpsProviderPluginBase {
    * @return void
    */
   public function server_type() {
-    $options = [];
-    $results = vpsCall('hetzner', 'server_types');
-    foreach($results['server_types'] as $s) {
-      $options[$s['id']] = implode(', ', [
-        $s['description'],
-        $s['architecture'],
-        $s['cpu_type'] . ' CPU',
-        $s['cores'] . ' cores',
-        $s['disk'] . ' GB storage',
-        $s['memory'] . ' GB memory',
-      ]);
+    $locations = vpsCall('hetzner', 'locations');
+    $response = vpsCall('hetzner', 'server_types');
+    $server_types = array_column($response['server_types'], 'description', 'id');
+    $processed_server_types = [];
+    foreach ($locations['locations'] as $lk => $lv) {
+      foreach ($server_types as $key => $value) {
+        $arch = array_column($response['server_types'], 'architecture', 'id');
+        $processed_value = $value . ' - ' . $arch[$key];
+
+        $prices = array_column($response['server_types'], 'prices', 'id');
+        if (!isset($prices[$key][$lk])) {
+          continue; // Skip if no price is available for current location.
+        }
+        $price = $prices[$key][$lk]['price_monthly']['gross'];
+        if (empty($price)) {
+          continue; // Skip if no price is available.
+        }
+        $processed_value .= ' - ' . number_format($price, 4) . ' EUR/month';
+
+        $cores = array_column($response['server_types'], 'cores', 'id');
+        $processed_value .= ', ' . $cores[$key] . ' cores';
+
+        $memory = array_column($response['server_types'], 'memory', 'id');
+        $processed_value .= ', ' . $memory[$key] . ' GB RAM';
+
+        $disk = array_column($response['server_types'], 'disk', 'id');
+        $processed_value .= ', ' . $disk[$key] . ' GB SSD';
+
+        $cpu_type = array_column($response['server_types'], 'cpu_type', 'id');
+        $processed_value .= ', ' . $cpu_type[$key] . ' CPU';
+
+        # Key format: 'server type ID'_'location ID'
+        $location_key = $lv['city'] . ', ' . $lv['country'] . ' (' . $lv['network_zone'] . ')';
+        $processed_key = implode('_', [$key, $lv['id']]);
+
+        $processed_server_types[$location_key][$processed_key] = $processed_value;
+      }
     }
-    return $options;
+    return $processed_server_types;
   }
 
   /**
