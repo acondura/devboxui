b4e9af0e948934f61e28d4bed749b39568fd026c
Andrei Condurachi <andrei@condurachi.ro>
2026-04-05 14:05:21 +0300

    homepage

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,13 @@
+export const runtime = 'edge';
+
 import { headers } from 'next/headers';
 import { DashboardView } from '@/modules/access/components/DashboardView';
 import { redirect } from 'next/navigation';
 
 export default async function DashboardPage() {
-  // Add 'await' here because Next.js 15 makes this async
   const headersList = await headers();
   const userEmail = headersList.get('x-user-email');
 
-  // Hard-fail safeguard
   if (!userEmail) {
     redirect('/');
   }
