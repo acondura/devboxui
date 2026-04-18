78b162ebd085e67d0f9858946223e2065b9f2217
Andrei Condurachi <andrei@condurachi.ro>
2026-04-18 20:40:59 +0300

    refactor: move OpenNext Cloudflare initialization to the top of next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
+
+// Enable calling `getCloudflareContext()` in `next dev`.
+initOpenNextCloudflareForDev();
 
 const nextConfig: NextConfig = {
 	/* config options here */
 };
 
 export default nextConfig;
-
-// Enable calling `getCloudflareContext()` in `next dev`.
-// See https://opennext.js.org/cloudflare/bindings#local-access-to-bindings.
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-initOpenNextCloudflareForDev();
