89a114c883105238ed6747363bbc81a464b9da84
Andrei Condurachi <andrei@condurachi.ro>
2026-04-19 08:44:25 +0300

    chore: configure wrangler project and update open-next wrapper to cloudflare

--- open-next.config.ts
+++ open-next.config.ts
@@ -3,7 +3,7 @@ import type { OpenNextConfig } from "@opennextjs/cloudflare";
 const config: OpenNextConfig = {
   default: {
     override: {
-      wrapper: "cloudflare-node",
+      wrapper: "cloudflare",
       converter: "edge",
       proxyExternalRequest: "fetch",
       incrementalCache: "dummy",
