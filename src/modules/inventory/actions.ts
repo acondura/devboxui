a1d9bfa078bedbdea3569298dd37b17b4daf9b64
Andrei Condurachi <andrei@condurachi.ro>
2026-04-24 22:33:15 +0300

    fix: update sudoers configuration to grant permissions to user abc instead of andrei

--- src/modules/inventory/actions.ts
+++ src/modules/inventory/actions.ts
@@ -273,7 +273,7 @@ done
 
 docker exec -d -u root code-server bash -c "
     chmod 666 /var/run/docker.sock
-    echo \"andrei ALL=(ALL) NOPASSWD:ALL\" >> /etc/sudoers
+    echo \"abc ALL=(ALL) NOPASSWD:ALL\" >> /etc/sudoers
     mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo \"deb [arch=\$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu noble stable\" > /etc/apt/sources.list.d/docker.list
